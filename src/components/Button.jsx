0715164b8edfb61bb523246d9136f74abee904aa
Yogesh_code_pirates <chaudhariy097@gmail.com>
2024-03-06 16:47:12 +0530

    changes practice

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+
+import PropTypes from 'prop-types'
+const Button = ({children ,onClick}) => {
+  return (
+    <button onClick={onClick}>{children}</button>
+  )
+}
+
+Button.propTypes={
+    children: PropTypes.node.isRequired,
+    onClick:PropTypes.func
+}
+export default Button
---
+++ src/data/example.js
@@ -0,0 +1,20 @@
+ const data={
+    Yogesh:{
+        title:'very hard',
+        decription:'no way'
+    },
+    Vhymal:{
+        title:'very cool',
+        decription:'kol mery'
+    },
+    Vivek:{
+        title:'very nice',
+        decription:'nnot give'
+    },
+    Harshal:{
+        title:'very go',
+        decription:'not jusdge'
+    }
+
+}
+export default data
